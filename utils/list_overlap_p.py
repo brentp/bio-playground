15bb92289e3a5e17d8b22ef7140290baf11972b7
Brent Pedersen (brentp) <bpederse@gmail.com>
2011-05-10 07:56:45 -0600

    list_overlap_p can take lists of gene-names rather than just numbers.

--- utils/list_overlap_p.py
+++ utils/list_overlap_p.py
@@ -14,11 +14,22 @@
     gives the probability that, 2 random gene subsets (chosen from 30000 genes)
     of length 345 and 322 would share at least 10 genes by chance.
 
+
+    This can also be called with 3 files of gene_names:
+
+        $ %prog all_genes.txt A_genes.txt B_genes.txt
+
+    A_genes.txt and B_genes.txt are then intersected to get the numbers for
+    the hypergeometric test. Each file must be a single column containing
+    the gene-name. The comparison *is* case sensitive.
+
+
     See: http://www.nslij-genetics.org/wli/pub/ieee-embs06.pdf
 
 """
 import optparse
 import sys
+import os.path as op
 import scipy.stats as ss
 
 def hypergeom(m, n, n1, n2, p=False):
@@ -50,15 +61,46 @@ def hypergeom(m, n, n1, n2, p=False):
     mmax = min(n1, n2)
     return ss.hypergeom.cdf(mmax, n, n1, n2) - ss.hypergeom.cdf(mmin, n, n1, n2)
 
+def with_genes(fftot, ffa, ffb):
+    """
+    given 3 genelists, calculate the p-value of the shared
+    genes between fa and fb that are drawn from ftot.
+    """
+
+    ftot = frozenset(f.strip() for f in open(fftot) if f.strip())
+    fa = frozenset(f.strip() for f in open(ffa) if f.strip())
+    fb = frozenset(f.strip() for f in open(ffb) if f.strip())
+
+    n1, n2 = len(fa), len(fb)
+    m = len(fa.intersection(fb))
+    n = len(ftot)
+
+    print "A     : %-32s:%-5i" % (ffa, n1)
+    print "B     : %-32s:%-5i" % (ffb, n2)
+    print "total : %-32s:%-5i" % (fftot, n)
+    print "shared: %-32s:%-5i" % (' ', m)
+    return hypergeom(m, n, n1, n2)
+
 
 def main():
     p = optparse.OptionParser(__doc__)
     opts, args = p.parse_args()
-    if (len(args) != 4):
+    if (len(args) not in (3, 4)):
         sys.exit(not p.print_help())
-    args = map(long, args)
-    m, n, n1, n2 = args
-    print hypergeom(m, n, n1, n2)
+    if len(args) == 4 and not all(a.isdigit() for a in args):
+        print >>sys.stderr, "four arguments must be integers"
+        sys.exit(not p.print_help())
+    elif len(args) == 3 and not all(op.exists(f) for f in args):
+        sys.exit(not p.print_help())
+
+    if len(args) == 4:
+        args = map(long, args)
+        m, n, n1, n2 = args
+        print hypergeom(m, n, n1, n2)
+    else:
+        tot_genes, a_genes, b_genes = map(str.strip, args)
+        print with_genes(tot_genes, a_genes, b_genes)
+
 
 
 if __name__ == "__main__":
